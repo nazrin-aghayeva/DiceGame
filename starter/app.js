96b95bcc48a15058a277e72d8e3b9074988d7ff1
nazrin <nazrinaghayeva@gmail.com>
2020-12-07 00:04:52 +0400

    -- Final Version

--- starter/app.js
+++ starter/app.js
@@ -2,7 +2,7 @@
 GAME RULES:
 
 - The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- In each turn, a player rolls a dice as many times as he wishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
@@ -73,7 +73,6 @@ function nextPlayer(){
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active')
 
-    // document.querySelector('.dice').style.display='none';
 }
 
 document.querySelector('.btn-new').addEventListener('click', init);
@@ -91,7 +90,7 @@ document.getElementById('score-0').textContent='0';
 document.getElementById('score-1').textContent='0';
 document.getElementById('current-0').textContent='0';
 document.getElementById('current-1').textContent='0';
-document.getElementById('input-score').textContent='0';
+document.getElementById('input-score').textContent= '0';
 document.querySelector('#name-0').textContent=" Player 1";
 document.querySelector('#name-1').textContent=" Player 2";
 document.querySelector('.player-0-panel').classList.remove('winner');
